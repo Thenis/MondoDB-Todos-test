6c1c1ef9f8a5bf624da2e7a46ccf0b239e2f767f
Thenis <d.stoilov96@gmail.com>
2017-05-21 18:03:03 +0300

    Add tests for GET /users/me and POST /users

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,25 +1,28 @@
 const expect = require("expect");
 const request = require("supertest");
-const {ObjectId} = require("mongodb");
+const {
+	ObjectId
+} = require("mongodb");
 
-const {app} = require("./../server");
-const {Todo} = require("./../models/todo");
+const {
+	app
+} = require("./../server");
+const {
+	Todo
+} = require("./../models/todo");
+const {
+	User
+} = require("./../models/user")
+const {
+	todos,
+	populateTodos,
+	users,
+	populateUsers
+} = require("./seed/seed");
 
-let todos = [{
-	_id: new ObjectId(),
-	text: "First test todo"
-}, {
-	_id: new ObjectId(),
-	text: "Second test todo",
-	completed: true,
-	completedAt: 333
-}];
 
-beforeEach((done) => {
-	Todo.remove({}).then(() => {
-		return Todo.insertMany(todos);
-	}).then(() => done());
-});
+beforeEach(populateUsers);
+beforeEach(populateTodos);
 
 describe("POST /todos", () => {
 	it("should create a new todo", (done) => {
@@ -27,17 +30,21 @@ describe("POST /todos", () => {
 
 		request(app)
 			.post("/todos")
-			.send({text})
+			.send({
+				text
+			})
 			.expect(200)
 			.expect((res) => {
 				expect(res.body.text).toBe(text);
 			})
 			.end((err, res) => {
-				if(err) {
+				if (err) {
 					return done(err);
 				}
 
-				Todo.find({text}).then((todos) => {
+				Todo.find({
+					text
+				}).then((todos) => {
 					expect(todos.length).toBe(1);
 					expect(todos[0].text).toBe(text);
 					done();
@@ -52,7 +59,7 @@ describe("POST /todos", () => {
 			.send({})
 			.expect(400)
 			.end((err, res) => {
-				if(err) {
+				if (err) {
 					return done(err);
 				}
 
@@ -154,7 +161,10 @@ describe("PATCH /todos/:id", () => {
 
 		request(app)
 			.patch(`/todos/${id}`)
-			.send({text: "Updated with test", completed: true})
+			.send({
+				text: "Updated with test",
+				completed: true
+			})
 			.expect(200)
 			.expect((doc) => {
 				expect(doc.body.todo.text).toBe("Updated with test");
@@ -169,7 +179,10 @@ describe("PATCH /todos/:id", () => {
 
 		request(app)
 			.patch(`/todos/${id}`)
-			.send({text: "Updated with test number 2", completed: false})
+			.send({
+				text: "Updated with test number 2",
+				completed: false
+			})
 			.expect(200)
 			.expect((doc) => {
 				expect(doc.body.todo.text).toBe("Updated with test number 2");
@@ -179,6 +192,85 @@ describe("PATCH /todos/:id", () => {
 			})
 			.end(done);
 	});
+});
 
+describe("GET /users/me", () => {
+	it("should return user if authenticated", (done) => {
+		request(app)
+			.get("/users/me")
+			.set("x-auth", users[0].tokens[0].token)
+			.expect(200)
+			.expect((res) => {
+				expect(res.body._id).toBe(users[0]._id.toHexString());
+				expect(res.body.email).toBe(users[0].email);
+			})
+			.end(done);
+	});
 
+	it("should return 404 if not authenticated", (done) => {
+		request(app)
+			.get("/users/me")
+			.expect(401)
+			.expect((res) => {
+				expect(res.body).toEqual({});
+			})
+			.end(done);
+	});
+});
+
+describe("POST /users", () => {
+	it("should create a user", (done) => {
+		let email = "example@example.com";
+		let password = "123asd";
+
+		request(app)
+			.post("/users")
+			.send({
+				email,
+				password
+			})
+			.expect(200)
+			.expect((res) => {
+				expect(res.headers["x-auth"]).toExist();
+				expect(res.body._id).toExist();
+				expect(res.body.email).toBe(email);
+			})
+			.end((err) => {
+				if (err) {
+					return done(err);
+				}
+
+				User.findOne({
+					email
+				}).then((user) => {
+					expect(user).toExist();
+					expect(user.password).toNotBe(password);
+					done();
+				})
+			});
+	});
+
+	it("should return validation errors if request invalid", (done) => {
+		let invalidEmail = "asd";
+		let invalidPass = "123"
+		request(app)
+			.post("/users")
+			.send({
+				email: invalidEmail,
+				password: invalidPass
+			})
+			.expect(400)
+			.end(done);
+	});
+
+	it("should not create user if email in use", (done) => {
+		request(app)
+			.post("/users")
+			.send({
+				email: users[0].email,
+				password: "pass123"
+			})
+			.expect(400)
+			.end(done);
+	});
 });
